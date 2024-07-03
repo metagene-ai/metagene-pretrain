607822c8d9854f0b82629fd3702feb61cfe80977
willieneis <willie.neiswanger@gmail.com>
2024-07-03 02:02:24 +0000

    uncomment torch.compile, fix spelling

--- train/litgpt/pretrain.py
+++ train/litgpt/pretrain.py
@@ -183,7 +183,7 @@ def main(
     fabric.print(f"Time to instantiate model: {time.perf_counter() - t0:.02f} seconds.")
     fabric.print(f"Total parameters: {num_parameters(model):,}")
 
-    # model = torch.compile(model)
+    model = torch.compile(model)
     model = fabric.setup(model)
     optimizer = torch.optim.AdamW(
         model.parameters(),
