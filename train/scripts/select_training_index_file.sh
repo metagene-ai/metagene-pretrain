23893206a6bf194970fc96bb0d2636ef9d16b40f
Willie Neiswanger <willie.neiswanger@gmail.com>
2024-08-12 01:43:27 +0300

    Continue training after switching to new index file (#32)
    * allow for continuing training with new index.json file
    
    * update scripts/select_training_index_file.sh to select second quarter-chunk index file
    
    * update README.md instructions

--- train/scripts/select_training_index_file.sh
+++ train/scripts/select_training_index_file.sh
@@ -6,7 +6,7 @@
 #aws s3 cp s3://mgfm-bucket-01/streams/index_files/index_87.json s3://mgfm-bucket-01/streams/index.json
 
 # To switch to "quarter-sized" training data files, run the following command:
-aws s3 cp s3://mgfm-bucket-01/streams/index_files/index_26a.json s3://mgfm-bucket-01/streams/index.json
-#aws s3 cp s3://mgfm-bucket-01/streams/index_files/index_26b.json s3://mgfm-bucket-01/streams/index.json
+#aws s3 cp s3://mgfm-bucket-01/streams/index_files/index_26a.json s3://mgfm-bucket-01/streams/index.json
+aws s3 cp s3://mgfm-bucket-01/streams/index_files/index_26b.json s3://mgfm-bucket-01/streams/index.json
 #aws s3 cp s3://mgfm-bucket-01/streams/index_files/index_26c.json s3://mgfm-bucket-01/streams/index.json
 #aws s3 cp s3://mgfm-bucket-01/streams/index_files/index_26d.json s3://mgfm-bucket-01/streams/index.json
